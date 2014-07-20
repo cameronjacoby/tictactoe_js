88f50644ddf370cf1834c45ea239579ee41fef22
cameronjacoby <cameron.jacoby@gmail.com>
2014-07-20 15:50:41 -0700

    refactored to consolidate code

--- script.js
+++ script.js
@@ -16,161 +16,44 @@ window.onload = function() {
   var boxEight = document.getElementById("eight");
   var boxNine = document.getElementById("nine");
 
-  // set counter variable
-
-  var count = 0;
-
-  // set variable for reset button
-
-  var resetButton = document.getElementById("reset_button");
-
   // set variables for game state and player state
 
   var gameState = document.getElementById("game_state");
   var playerState = document.getElementById("player_state");
 
-  // set variables for types of game
+  // set variables game types
 
   var singlePlayer = document.getElementById("single_player");
   var twoPlayer = document.getElementById("two_player");
 
+  // set variable for reset button
 
-  ////////////////////
+  var resetButton = document.getElementById("reset_button");
 
-  // TWO PLAYER GAME
+  // function checks for win
 
-  twoPlayer.onclick = function (event) {
-    playerState.innerHTML = "You have chosen to play the game with two players. Click below to switch to one player.";
-    twoPlayer.style.display = "none";
-    singlePlayer.style.display = "block";
-    boxOne.innerHTML = "";
-    boxTwo.innerHTML = "";
-    boxThree.innerHTML = "";
-    boxFour.innerHTML = "";
-    boxFive.innerHTML = "";
-    boxSix.innerHTML = "";
-    boxSeven.innerHTML = "";
-    boxEight.innerHTML = "";
-    boxNine.innerHTML = "";
-    count = 0;
-
-    // initial game state --> player1's turn
-
-    var player1 = true;
-    var player2 = false;
-    gameState.innerHTML = "Start new game. It's player X's turn!";
-
-    // sets click event for each box
-
-    for (var i = 0; i < 9; i += 1) {
-      boxes[i].onclick = function (event) {
-
-        // resets board on click if a winner was just declared
-        if (checkFunction("X") === true || checkFunction("O") === true) {
-          resetBoard();
-          return;
-        }
-
-        // resets game on click if the game is finished and there was no winner
-        else if (count === 9 && checkFunction("X") === false && checkFunction("O") === false) {
-          resetBoard();
-          return;
-        }
-
-        // requires a blank box for the player to make a move
-        else if (this.innerHTML === "") {
-
-          // if it's player 1's turn
-          if (player1 === true) {
-            gameState.innerHTML = "It's player O's turn!"
-            this.style.color = '#00AEBC';
-            this.innerHTML = "X";
-            count += 1;
-            console.log(count);
-
-            if (count > 4) {
-              checkFunction("X");
-              checkFunction("O");
-
-              if (count === 9 && checkFunction("X") === false && checkFunction("O") === false) {
-                gameState.innerHTML = "There is no winner! Click the reset button or anywhere on the board to start a new game.";
-              }
-            }
-
-            player1 = false;
-            player2 = true;
-          }
-
-          // if it's player2's turn
-          else {
-            gameState.innerHTML = "It's player X's turn!"
-            this.style.color = '#94D60A';
-            this.innerHTML = "O";
-            count += 1;
-            console.log(count);
-
-            if (count > 4) {
-              checkFunction("X");
-              checkFunction("O");
-
-              if (count === 9 && checkFunction("X") === false && checkFunction("O") === false) {
-                gameState.innerHTML = "There is no winner! Click the reset button or anywhere on the board to start a new game.";
-              }
-            }
-
-            player2 = false;
-            player1 = true;
-          }
-        }
-
-        // if the box clicked is not blank, do nothing
-        else {
-          console.log(count);
-        }
-      };
-    }
-
-    // function to check for a winner
-
-    var checkFunction = function(S) {
+    var checkWin = function(S) {
       var boardMap = [ [boxOne.innerHTML, boxTwo.innerHTML, boxThree.innerHTML], [boxFour.innerHTML, boxFive.innerHTML, boxSix.innerHTML], [boxSeven.innerHTML, boxEight.innerHTML, boxNine.innerHTML] ];
-      var winnerString = "Player " + S + " is the winner! Click the reset button or anywhere on the board to start a new game.";
 
-      // checks for row wins
-      if (boardMap[0][0] === S && boardMap[0][1] === S && boardMap[0][2] === S) {
-        gameState.innerHTML = winnerString
+      if (boardMap[0][0] === S && 
+        ((boardMap[0][1] === S && boardMap[0][2] === S) || 
+        (boardMap[1][0] === S && boardMap[2][0] === S) || 
+        (boardMap[1][1] === S && boardMap[2][2] === S))) {
+        return true;
+      }
+      else if (boardMap[0][2] === S && 
+        ((boardMap[1][2] === S && boardMap[2][2] === S) || 
+        (boardMap[1][1] === S && boardMap[2][0] === S))) {
         return true;
       }
       else if (boardMap[1][0] === S && boardMap[1][1] === S && boardMap[1][2] === S) {
-        gameState.innerHTML = winnerString
         return true;
       }
       else if (boardMap[2][0] === S && boardMap[2][1] === S && boardMap[2][2] === S) {
-        gameState.innerHTML = winnerString
         return true;
       }
 
-      // checks for column wins
-      else if (boardMap[0][0] === S && boardMap[1][0] === S && boardMap[2][0] === S) {
-        gameState.innerHTML = winnerString
-        return true;
-      }
       else if (boardMap[0][1] === S && boardMap[1][1] === S && boardMap[2][1] === S) {
-        gameState.innerHTML = winnerString
-        return true;
-      }
-      else if (boardMap[0][2] === S && boardMap[1][2] === S && boardMap[2][2] === S) {
-        gameState.innerHTML = winnerString
-        return true;
-      }
-
-      // checks for diagonal wins
-      else if (boardMap[0][0] === S && boardMap[1][1] === S && boardMap[2][2] === S) {
-        gameState.innerHTML = winnerString
-        return true;
-      }
-      else if (boardMap[0][2] === S && boardMap[1][1] === S && boardMap[2][0] === S) {
-        gameState.innerHTML = winnerString
         return true;
       }
 
@@ -180,27 +63,118 @@ window.onload = function() {
       }
     };
 
-    // reset function
+
+  ////////////////////
+
+  // two player game
+
+  twoPlayer.onclick = function (event) {
+    playerState.innerHTML = "You have chosen to play the game with two players. Click below to switch to one player.";
+    twoPlayer.style.display = "none";
+    singlePlayer.style.display = "block";
+    for (var i = 0; i < boxes.length; i += 1) {
+      boxes[i].innerHTML = "";
+    }
+    var count = 0;
+
+    // initial game state --> playerX's turn
+
+    var playerX = true;
+    var playerO = false;
+    gameState.innerHTML = "Start new game. It's player X's turn!";
+
+    // function executes win
+
+    var executeWin = function(S) {
+      gameState.innerHTML = "Player " + S + " is the winner! Click the reset button or anywhere on the board to start a new game.";
+    };
+
+    // function resets board
 
     var resetBoard = function() {
-      boxOne.innerHTML = "";
-      boxTwo.innerHTML = "";
-      boxThree.innerHTML = "";
-      boxFour.innerHTML = "";
-      boxFive.innerHTML = "";
-      boxSix.innerHTML = "";
-      boxSeven.innerHTML = "";
-      boxEight.innerHTML = "";
-      boxNine.innerHTML = "";
+      for (var i = 0; i < boxes.length; i += 1) {
+        boxes[i].innerHTML = "";
+      }
       count = 0;
-      console.log(count);
-      if (player1 === true) {
+      if (playerX) {
         gameState.innerHTML = "Start new game. It's player X's turn!"
       }
       else {
         gameState.innerHTML = "Start new game. It's player O's turn!"
       }
+      return;
     };
+
+    // set click event for each box
+
+    for (var i = 0; i < 9; i += 1) {
+      boxes[i].onclick = function (event) {
+
+        // reset board on click if a winner was just declared
+        if (checkWin("X") || checkWin("O")) {
+          resetBoard();
+        }
+
+        // reset board on click if a tie was just declared
+        else if (count === 9 && !(checkWin("X")) && !(checkWin("O"))) {
+          resetBoard();
+        }
+
+        // require a blank box for player to make a move
+        else if (this.innerHTML === "") {
+
+          // if playerX's turn
+          if (playerX) {
+            gameState.innerHTML = "It's player O's turn!"
+            // good place to use className to add a pre-styled "X" class; same thing for "O"
+            this.style.color = '#00AEBC';
+            this.innerHTML = "X";
+            count += 1;
+
+            if (checkWin("X")) {
+              executeWin("X");
+            }
+            else if (checkWin("O")) {
+              executeWin("O");
+            }
+            else if (count === 9) {
+              gameState.innerHTML = "There is no winner! Click the reset button or anywhere on the board to start a new game.";
+            }
+
+            playerX = false;
+            playerO = true;
+          }
+
+          // if playerO's turn --> good place to use a function since this is repetitive with playerX
+          else {
+            gameState.innerHTML = "It's player X's turn!"
+            this.style.color = '#94D60A';
+            this.innerHTML = "O";
+            count += 1;
+
+            if (checkWin("X")) {
+              executeWin("X");
+            }
+            else if (checkWin("O")) {
+              executeWin("O");
+            }
+            else if (count === 9) {
+              gameState.innerHTML = "There is no winner! Click the reset button or anywhere on the board to start a new game.";
+            }
+
+            playerO = false;
+            playerX = true;
+          }
+        }
+
+        // if box clicked is not blank, do nothing
+        else {
+          return;
+        }
+      };
+    }
+
+    // set click event for reset button
 
     resetButton.onclick = function (event) {
       resetBoard();
@@ -210,71 +184,93 @@ window.onload = function() {
 
   ////////////////////
 
-  // SINGLE PLAYER GAME
+  // single player game
 
   singlePlayer.onclick = function (event) {
 
     playerState.innerHTML = "You have chosen to play the game with one player. Click below to switch to two players.";
     singlePlayer.style.display = "none";
     twoPlayer.style.display = "block";
-    boxOne.innerHTML = "";
-    boxTwo.innerHTML = "";
-    boxThree.innerHTML = "";
-    boxFour.innerHTML = "";
-    boxFive.innerHTML = "";
-    boxSix.innerHTML = "";
-    boxSeven.innerHTML = "";
-    boxEight.innerHTML = "";
-    boxNine.innerHTML = "";
-    count = 0;
+    for (var i = 0; i < boxes.length; i += 1) {
+      boxes[i].innerHTML = "";
+    }
+    var count = 0;
 
     // initial game state --> your turn
 
     gameState.innerHTML = "Start new game. It's your turn!"
     var computer = false;
 
-    // sets click event for each box
+    // function executes win
+
+    var executeWin = function(S) {
+      if (S === "X") {
+        gameState.innerHTML = "Congratulations, you beat the computer! Click the reset button or anywhere on the board to start a new game.";
+      }
+      else {
+        gameState.innerHTML = "Looks like the computer has outsmarted you this time! Click the reset button or anywhere on the board to start a new game.";
+      }
+    };
+
+    // function resets board
+
+    var resetBoard = function() {
+      if (checkWin("X")) {
+        gameState.innerHTML = "Start new game. The computer will make the first move.";
+        setTimeout(function() {
+          boxFive.style.color = '#94D60A';
+          boxFive.innerHTML = "O";
+          gameState.innerHTML = "It's your turn!"
+          count += 1;
+        }, 1000);
+      }
+      else {
+        gameState.innerHTML = "Start new game. It's your turn!";
+      }
+      for (var i = 0; i < boxes.length; i += 1) {
+        boxes[i].innerHTML = "";
+      }
+      count = 0;
+      return;
+    };
+
+    // set click event for each box
 
     for (var i = 0; i < 9; i += 1) {
       boxes[i].onclick = function (event) {
 
-        // resets board on click if a winner was just declared
-        if (checkFunction("X") === true || checkFunction("O") === true) {
+        // reset board on click if a winner was just declared
+        if (checkWin("X") || checkWin("O")) {
           resetBoard();
-          return;
         }
 
-        // resets game on click if the game is finished and there was no winner
-        else if (count === 9 && checkFunction("X") === false && checkFunction("O") === false) {
+        // reset board on click if a tie was just declared
+        else if (count === 9 && !(checkWin("X")) && !(checkWin("O"))) {
           resetBoard();
-          return;
         }
 
-        // requires a blank box for the player to make a move
+        // require a blank box for player to make a move
         else if (this.innerHTML === "") {
-
           gameState.innerHTML = "Now the computer will make its move!"
           this.style.color = '#00AEBC';
           this.innerHTML = "X";
           count += 1;
-          console.log(count);
 
-          if (count > 4) {
-            checkFunction("X");
+          if (checkWin("X")) {
+            executeWin("X");
+          }
 
-            if (count === 9 && checkFunction("X") === false && checkFunction("O") === false) {
-              gameState.innerHTML = "There is no winner! Click the reset button or anywhere on the board to start a new game.";
-              return;
-            }
+          else if (count === 9) {
+            gameState.innerHTML = "There is no winner! Click the reset button or anywhere on the board to start a new game.";
+            return;
           }
 
           setTimeout(function() {
-
-            if (checkFunction("X") === true || checkFunction("O") === true) {
+            if (checkWin("X") || checkWin("O")) {
               return;
             }
 
-            // first, computer checks for a possible win
+            // first, computer checks for possible win
 
             else if (boxOne.innerHTML === "O" && boxTwo.innerHTML === "O" && boxThree.innerHTML === "") {boxThree.style.color = '#94D60A'; boxThree.innerHTML = "O";}
             else if (boxOne.innerHTML === "O" && boxThree.innerHTML === "O" && boxTwo.innerHTML === "") {boxTwo.style.color = '#94D60A'; boxTwo.innerHTML = "O";}
@@ -328,7 +324,7 @@ window.onload = function() {
             else if (boxThree.innerHTML === "X" && boxSeven.innerHTML === "X" && boxFive.innerHTML === "") {boxFive.style.color = '#94D60A'; boxFive.innerHTML = "O";}
             else if (boxFive.innerHTML === "X" && boxSeven.innerHTML === "X" && boxThree.innerHTML === "") {boxThree.style.color = '#94D60A'; boxThree.innerHTML = "O";}
 
-            // if no possible win and no opportunities to block the user, the computer chooses the first available box
+            // if no possible win and no opportunities to block, computer chooses first available box
 
             else if (boxOne.innerHTML === "") {
               boxOne.style.color = '#94D60A';
@@ -369,111 +365,26 @@ window.onload = function() {
 
             gameState.innerHTML = "It's your turn!"
             count += 1;
-            console.log(count);
-            if (count > 4) {
-              checkFunction("O");
-              if (count === 9 && checkFunction("X") === false && checkFunction("O") === false) {
-                gameState.innerHTML = "There is no winner! Click the reset button or anywhere on the board to start a new game.";
-                return;
+
+            if (checkWin("O")) {
+              executeWin("O");
             }
+
+            else if (count === 9) {
+              gameState.innerHTML = "There is no winner! Click the reset button or anywhere on the board to start a new game.";
+              return;
             }
           }, 1000);
         }
 
-        // if the box clicked is not blank, do nothing
+        // if box clicked is not blank, do nothing
         else {
-          console.log(count);
+          return;
         }
       };
     }
 
-    // function to check for a winner
-
-    var checkFunction = function(S) {
-      var boardMap = [ [boxOne.innerHTML, boxTwo.innerHTML, boxThree.innerHTML], [boxFour.innerHTML, boxFive.innerHTML, boxSix.innerHTML], [boxSeven.innerHTML, boxEight.innerHTML, boxNine.innerHTML] ];
-      var winnerString = function() {
-        if (S === "X") {
-          gameState.innerHTML = "Congratulations, you beat the computer! Click the reset button or anywhere on the board to start a new game.";
-        }
-        else {
-          gameState.innerHTML = "Looks like the computer has outsmarted you this time! Click the reset button or anywhere on the board to start a new game.";
-        };
-      }
-
-      // checks for row wins
-      if (boardMap[0][0] === S && boardMap[0][1] === S && boardMap[0][2] === S) {
-        winnerString();
-        return true;
-      }
-      else if (boardMap[1][0] === S && boardMap[1][1] === S && boardMap[1][2] === S) {
-        winnerString();
-        return true;
-      }
-      else if (boardMap[2][0] === S && boardMap[2][1] === S && boardMap[2][2] === S) {
-        winnerString();
-        return true;
-      }
-
-      // checks for column wins
-      else if (boardMap[0][0] === S && boardMap[1][0] === S && boardMap[2][0] === S) {
-        winnerString();
-        return true;
-      }
-      else if (boardMap[0][1] === S && boardMap[1][1] === S && boardMap[2][1] === S) {
-        winnerString();
-        return true;
-      }
-      else if (boardMap[0][2] === S && boardMap[1][2] === S && boardMap[2][2] === S) {
-        winnerString();
-        return true;
-      }
-
-      // checks for diagonal wins
-      else if (boardMap[0][0] === S && boardMap[1][1] === S && boardMap[2][2] === S) {
-        winnerString();
-        return true;
-      }
-      else if (boardMap[0][2] === S && boardMap[1][1] === S && boardMap[2][0] === S) {
-        winnerString();
-        return true;
-      }
-
-      // else, there is no winner
-      else {
-        return false;
-      }
-
-    };
-
-    // reset function
-
-    var resetBoard = function() {
-      console.log(checkFunction("X"));
-      if (checkFunction("X") === true) {
-        gameState.innerHTML = "Start new game. The computer will make the first move.";
-        setTimeout(function() {
-          boxFive.style.color = '#94D60A';
-          boxFive.innerHTML = "O";
-          gameState.innerHTML = "It's your turn!"
-          count += 1;
-          console.log(count);
-        }, 1000);
-      }
-      else {
-        gameState.innerHTML = "Start new game. It's your turn!";
-      }
-      boxOne.innerHTML = "";
-      boxTwo.innerHTML = "";
-      boxThree.innerHTML = "";
-      boxFour.innerHTML = "";
-      boxFive.innerHTML = "";
-      boxSix.innerHTML = "";
-      boxSeven.innerHTML = "";
-      boxEight.innerHTML = "";
-      boxNine.innerHTML = "";
-      count = 0;
-      console.log(count);
-    };
+    // set click event for reset button
 
     resetButton.onclick = function (event) {
       resetBoard();
@@ -481,3 +392,7 @@ window.onload = function() {
   }
 
 }
+
+
+
+
